5113b359fe9ae337be8bf9978e1cc16e2c2ae96c
naman goyal <namangoyal176@gmail.com>
2026-06-22 02:58:35 +0530

    Add files via upload

---
+++ lec23/main.js
@@ -0,0 +1,9 @@
+console.log("hii beta batch");
+   let subhas=require("./auth.js")
+console.log("bye");
+console.log(subhas);
+
+subhas.Auth();
+
+let operating=require("os");
+console.log(operating);
